168adf33503e33b1364ff30cb926f5e8f3efc57e
Teemu Kerkola <tokerk@utu.fi>
2022-08-05 21:44:15 +0300

    added product deletion endpoint

--- pages/api/db/products/[id].ts
+++ pages/api/db/products/[id].ts
@@ -1,8 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getProduct, updateProduct } from "prisma/product";
+import { deleteProduct, getProduct, updateProduct } from "prisma/product";
 
 const DATABASE_ACCESS_TOKEN = process.env.NEXT_PUBLIC_DATABASE_ACCESS_TOKEN;
+const PRISMIC_PRODUCTS_ENDPOINT = process.env.PRISMIC_PRODUCTS_ENDPOINT;
+const PRISMIC_PRODUCTS_TOKEN = process.env.PRISMIC_PRODUCTS_TOKEN;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
    // Authorize
@@ -47,6 +49,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
          return res.status(400).end();
       }
       console.log("Product " + id + " was updated.");
+      // Return product on success
+      return res.status(200).json(product);
+   }
+
+   if (req.method === "DELETE") {
+      // Delete product
+      const product = await deleteProduct(id);
+      console.log("Product " + id + " was deleted.");
+
+      // Delete Product from Prismic
+      const prismicRes = await fetch(`${PRISMIC_PRODUCTS_ENDPOINT}/deleteItems`, {
+         method: "POST",
+         headers: {
+            Authorization: `Bearer ${PRISMIC_PRODUCTS_TOKEN}`,
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify([id]),
+      });
+
+      // Log error
+      if (prismicRes.ok === false) {
+         console.error("Failed delete prismic product.");
+         return res.status(400).end();
+      }
       // Return product on success
       return res.status(200).json(product);
    }
